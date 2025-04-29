4a57593879625c1c2cbee15fd6e674376cd494c5
mrhl082024 <mrhl082024@gmail.com>
2025-04-29 10:30:18 +0200

    Add structure

--- structure/index.ts
+++ structure/index.ts
@@ -1,2 +1,12 @@
 import type { StructureResolver } from "sanity/structure";
 import { CalendarIcon, UserIcon, PinIcon } from "@sanity/icons";
+
+export const structure: StructureResolver = (S) =>
+    S.list()
+    .id("root")
+    .title("Content")
+    .items([
+        S.documentTypeListItem("event").title("Events").icon(CalendarIcon),
+        S.documentTypeListItem("artist").title("Artists").icon(UserIcon),
+        S.documentTypeListItem("venue").title("Venues").icon(PinIcon),
+    ])
