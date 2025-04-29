f6e7a27a73ff77a768cf41f50eea15199a45b302
mrhl082024 <mrhl082024@gmail.com>
2025-04-29 09:49:02 +0200

    add function to DoorsOpenInput

--- schemaTypes/components/DoorsOpenInput.tsx
+++ schemaTypes/components/DoorsOpenInput.tsx
@@ -1,4 +1,9 @@
-import {NumberInputProps} from 'sanity'
+import {NumberInputProps, useFormValue} from 'sanity'
+import {Stack, Text} from '@sanity/ui'
+
+function subtractMinutesFromDate(date: string, minutes: number) {
+  return new Date(new Date(date).getTime() - minutes * 60000)
+}
 
 export function DoorsOpenInput(props: NumberInputProps) {
   return <div style={{border: '1px solid red'}}>DoorsOpenInput</div>
