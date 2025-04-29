5ba2945cd9d1345a08cbdedeb3bd8f050325b513
mrhl082024 <mrhl082024@gmail.com>
2025-04-29 11:59:14 +0200

    ADD defaultDocumentNode

--- structure/defaultDocumentNode.ts
+++ structure/defaultDocumentNode.ts
@@ -1,1 +1,21 @@
 import type { DefaultDocumentNodeResolver } from "sanity/structure";
+import DocumentsPane from "sanity-plugin-documents-pane";
+
+export const defualtDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) =>{
+    switch (schemaType) {
+        case "artist":
+            return S.document().views([
+                S.view.form(),
+                S.view
+                    .component(DocumentsPane)
+                    .options({
+                        query: `*[_type == "event" && references($id)]`,
+                        params: {id: `_id`},
+                        options: {perspective: `previewDrafts`}
+                    })
+                    .title("Events")
+            ])
+            default:
+                return S.document().views([S.view.form()])
+    }
+}
