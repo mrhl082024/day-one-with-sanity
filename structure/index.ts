a017a8d38d4db116a291e5bc6eec7ba3ae4d81ec
mrhl082024 <mrhl082024@gmail.com>
2025-04-29 10:40:58 +0200

    EDIT structure/index

--- structure/index.ts
+++ structure/index.ts
@@ -6,7 +6,17 @@ export const structure: StructureResolver = (S) =>
     .id("root")
     .title("Content")
     .items([
-        S.documentTypeListItem("event").title("Events").icon(CalendarIcon),
+        S.listItem()
+        .title("Upcoming Events")
+        .schemaType("event")
+        .icon(CalendarIcon)
+        .child(S.documentList().title("Upcoming Events").filter("date >= now()")),
+        S.listItem()
+        .title("Past Events")
+        .schemaType("event")
+        .icon(CalendarIcon)
+        .child(S.documentList().title("Past Events").filter("date < now()")),
+        S.divider(),
         S.documentTypeListItem("artist").title("Artists").icon(UserIcon),
         S.documentTypeListItem("venue").title("Venues").icon(PinIcon),
     ])
